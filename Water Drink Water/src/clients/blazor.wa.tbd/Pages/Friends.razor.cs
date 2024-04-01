b4995b0c0999b54774c7e11b427b2dd014110458
Terry Burns-Dyson <terrybd@gmail.com>
2024-04-01 09:03:59 -0400

    Add Friends
    Introduced BlazorBootstrap library for modal
    New menu item - Friends
    Allow adding a friends group
    Show list of friends on screen

---
+++ Water Drink Water/src/clients/blazor.wa.tbd/Pages/Friends.razor.cs
@@ -0,0 +1,41 @@
+﻿using blazor.wa.tbd.Services;
+using BlazorBootstrap;
+using Microsoft.AspNetCore.Components;
+using viewmodels;
+
+namespace blazor.wa.tbd.Pages;
+
+public partial class Friends
+{
+    [Inject] FriendsService FriendsService { get; set; } = null!;
+
+    private Modal _modal = null!;
+    private string _groupName = string.Empty;
+
+    private IEnumerable<GroupViewModel> Groups { get; set; } = Array.Empty<GroupViewModel>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Groups = await FriendsService.GetGroups();
+    }
+
+    private async Task OnAddGroupClick()
+    {
+        if (await FriendsService.AddGroup(_groupName))
+        {
+            Groups = await FriendsService.GetGroups();
+            
+            await _modal.HideAsync();
+        }
+    }
+
+    private async Task OnShowModalClick()
+    {
+        await _modal.ShowAsync();
+    }
+
+    private async Task OnHideModalClick()
+    {
+        await _modal.HideAsync();
+    }
+}
---
+++ Water Drink Water/src/clients/blazor.wa.tbd/Services/FriendsService.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Blazored.LocalStorage;
+using viewmodels;
+
+namespace blazor.wa.tbd.Services;
+
+public class FriendsService(HttpClient client, ILocalStorageService localStorage)
+{
+    private readonly Lazy<ValueTask<string>> _token = new(value: localStorage.GetItemAsync<string>("token"));
+    private ValueTask<string> Token => _token.Value;
+
+    public async Task<bool> AddGroup(string name)
+    {
+        var token = await _token.Value;
+
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            return false;
+        }
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await client.PostAsJsonAsync("api/groups",
+            new { name });
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<IEnumerable<GroupViewModel>> GetGroups()
+    {
+        var token = await _token.Value;
+
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            return Array.Empty<GroupViewModel>();
+        }
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await client.GetAsync("api/groups");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return Array.Empty<GroupViewModel>();
+        }
+
+        return await response.Content.ReadFromJsonAsync<IEnumerable<GroupViewModel>>();
+    }
+}
