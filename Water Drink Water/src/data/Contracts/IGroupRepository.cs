a8ba324b97ebd99167294cbc6b748c96932c9eb8
Terry Burns-Dyson <terrybd@gmail.com>
2024-04-22 08:37:51 -0400

    Allow joining groups given code
    - User can not become member of their own group
    - User can not become member of same group twice
    
    Changed some userId to accountId to better reflect account being our "user" object

--- Water Drink Water/src/data/Contracts/IGroupRepository.cs
+++ Water Drink Water/src/data/Contracts/IGroupRepository.cs
@@ -5,6 +5,10 @@ namespace TbdFriends.WaterDrinkWater.Data.Contracts;
 public interface IGroupRepository
 {
     void Add(Group group);
-    Group? GetByName(string name, int userId);
-    IEnumerable<Group> GetGroups(int userId);
+    Group? GetByName(string name, int accountId);
+    IEnumerable<Group> GetGroups(int accountId);
+    Group? GetByCode(string code);
+    void AddMembership(Membership membership);
+
+    IEnumerable<Membership> GetMembershipsForGroup(int groupId);
 }
---
+++ Water Drink Water/tests/application.tests/ConcerningGroups/when_joining_a_group_and_already_joined.cs
@@ -0,0 +1,53 @@
+﻿using Ardalis.Result;
+using TbdFriends.WaterDrinkWater.Application.Contracts;
+
+namespace application.tests.ConcerningGroups;
+
+public class when_joining_a_group_and_already_joined
+{
+    [Fact]
+    public void membership_is_not_duplicated()
+    {
+        // Arrange
+        const int UserId = 10001;
+        const int GroupId = 20001;
+        const string GroupCode = "GroupCode";
+
+        var repository = Substitute.For<IGroupRepository>();
+        var generator = Substitute.For<ICodeGenerator>();
+
+        repository
+            .GetByCode(GroupCode)
+            .Returns(new Group()
+            {
+                Id = GroupId,
+                OwnerId = 10002
+            });
+
+        repository
+            .GetMembershipsForGroup(Arg.Is(GroupId))
+            .Returns(new List<Membership>
+            {
+                new()
+                {
+                    Id = 1,
+                    GroupId = GroupId,
+                    AccountId = UserId
+                }
+            });
+
+        var service = new GroupService(repository, generator);
+
+        // Act
+
+        var result = service.JoinGroup(GroupCode, UserId);
+
+        // Assert
+
+        repository
+            .DidNotReceive()
+            .AddMembership(Arg.Any<Membership>());
+
+        Assert.False(result);
+    }
+}
