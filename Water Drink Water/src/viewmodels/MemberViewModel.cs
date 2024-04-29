1ef4b1e847b4a05578749b277b1a693a80e98bb8
Terry Burns-Dyson <terrybd@gmail.com>
2024-04-29 07:51:35 -0400

    Display Member Progress in Fluent Progress bar for now

--- Water Drink Water/src/viewmodels/MemberViewModel.cs
+++ Water Drink Water/src/viewmodels/MemberViewModel.cs
@@ -4,5 +4,6 @@ public class MemberViewModel
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public int Progress { get; set; } = 0;
     public bool IsOwner { get; set; }
 }
